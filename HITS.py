eea6b0a06db4f38ecc730e1cbfe85855c93f5c65
Destiny0504 <105703044@mail2.nccu.tw>
2022-01-07 22:15:27 +0800

    finish page rank algorithm

--- HITS.py
+++ HITS.py
@@ -1,5 +1,4 @@
 import os
-from posixpath import expanduser
 import re
 
 def create_graph(data_file):
@@ -7,17 +6,19 @@ def create_graph(data_file):
     incoming = {} # incoming graph
 
     nodes = []
-    print(cur_file)
-    f = open(cur_file,'r')
+    f = open(data_file,'r')
     for data in f.readlines():
         start, dest = tuple(re.findall(r'([\d]+),([\d]+)', data)[0])
         nodes.append(start)
         nodes.append(dest)
         try:
             graph[start] += [dest]
-            incoming[dest] += [start]
         except:
             graph[start] = [dest]
+
+        try:
+            incoming[dest] += [start]
+        except:
             incoming[dest] = [start]
 
     return incoming, graph, list(set(nodes))
@@ -33,13 +34,14 @@ def HITS(incoming_graph, outgoing_graph, nodes, itr_times):
         total_authority = 0
         for node in nodes:
             graph_authority[node] = 0
-            graph_hub[node] = 0
             try:
                 for incoming in incoming_graph[node]:
                     graph_authority[node] += graph_hub[incoming]
                 total_authority += graph_authority[node]
             except:
                 pass
+        for node in nodes:
+            graph_hub[node] = 0
             try:
                 for outgoing in outgoing_graph[node]:
                     graph_hub[node] += graph_authority[outgoing]
@@ -53,16 +55,18 @@ def HITS(incoming_graph, outgoing_graph, nodes, itr_times):
 
 if __name__=="__main__":
     cur_file = os.path.abspath(__file__)
-    cur_file = re.findall(r'(.*)/[\w]*\.py', cur_file)[0] + '/hw3dataset/graph_6.txt'
+    cur_file = re.findall(r'(.*)/[\w]*\.py', cur_file)[0] + '/hw3dataset/graph_3.txt'
     total_hub = 0
     total_authority = 0
     incoming_graph, outgoing_graph, nodes = create_graph(cur_file)
 
-    graph_hub, graph_authority = HITS(incoming_graph, outgoing_graph, nodes, 10)
+    print(incoming_graph)
+    print(outgoing_graph)
+    graph_hub, graph_authority = HITS(incoming_graph, outgoing_graph, nodes, 50)
 
     nodes = sorted(list(nodes))
     for node in nodes:
-        print(f'node : {node},  hub : {graph_hub[node]}, authority : {graph_authority[node]}')
+        print(f'node : {node},  hub : {round(graph_hub[node],3)}, authority : {round(graph_authority[node],3)}')
         total_hub += graph_hub[node]
         total_authority += graph_authority[node]
     print(graph_hub)
---
+++ page_rank.py
@@ -0,0 +1,65 @@
+import os
+import re
+
+def create_graph(data_file):
+    graph = {} # outgoing graph
+    incoming = {} # incoming graph
+
+    nodes = []
+    f = open(data_file,'r')
+    for data in f.readlines():
+        start, dest = tuple(re.findall(r'([\d]+),([\d]+)', data)[0])
+        nodes.append(start)
+        nodes.append(dest)
+        try:
+            graph[start] += [dest]
+        except:
+            graph[start] = [dest]
+
+        try:
+            incoming[dest] += [start]
+        except:
+            incoming[dest] = [start]
+
+    return incoming, graph, list(set(nodes))
+
+def page_rank(incoming_graph, outgoing_graph, nodes, itr_times, damping_factor):
+    page_rank = {}
+    base = damping_factor / len(nodes)
+    factor = 1 - damping_factor
+    total_factor = 0
+    for key in nodes:
+        try:
+            page_rank[key] = 1 / outgoing_graph[key]
+        except:
+            page_rank[key] = 0
+
+    for _ in range(itr_times):
+        for node in nodes:
+            page_rank[node] = 0
+            page_rank[node] += base
+            try:
+                for incoming in incoming_graph[node]:
+                    page_rank[node] += (page_rank[incoming] / len(outgoing_graph[incoming])) * factor
+            except:
+                pass
+    for key in nodes:
+        total_factor += page_rank[key]
+
+    for key in nodes:
+        page_rank[key ] /= total_factor
+    return page_rank
+
+if __name__=="__main__":
+    cur_file = os.path.abspath(__file__)
+    cur_file = re.findall(r'(.*)/[\w]*\.py', cur_file)[0] + '/hw3dataset/graph_3.txt'
+    incoming_graph, outgoing_graph, nodes = create_graph(cur_file)
+
+    print(incoming_graph)
+    print(outgoing_graph)
+    page_rank = page_rank(incoming_graph, outgoing_graph, nodes, 50, 0.15)
+
+    nodes = sorted(list(nodes))
+    for node in nodes:
+        print(f'node : {node},  page_rank : {round(page_rank[node],3)}')
+    print(page_rank)
